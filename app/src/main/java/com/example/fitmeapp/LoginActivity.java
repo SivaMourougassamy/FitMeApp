1a74a5e0b77bb08668e2549f17f2d70df1c5f47a
ViNguyen15 <fluffywin@gmail.com>
2019-10-19 20:30:19 -0400

    Directing Buttons

--- app/src/main/java/com/example/fitmeapp/LoginActivity.java
+++ app/src/main/java/com/example/fitmeapp/LoginActivity.java
@@ -2,13 +2,33 @@ package com.example.fitmeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openUser();
+            }
+        });
+
+
+    }
+
+    public void openUser(){
+        Intent intent = new Intent(this, UserActivity.class);
+        startActivity(intent);
     }
 }
