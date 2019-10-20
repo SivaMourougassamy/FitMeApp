25a4a1f0eb31949addabee99c54a25cf53ca63c4
ViNguyen15 <fluffywin@gmail.com>
2019-10-19 22:08:28 -0400

    fixing the src

--- app/build/generated/source/buildConfig/debug/com/zybooks/fitme/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/zybooks/fitme/BuildConfig.java
@@ -1,7 +1,7 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.example.fitmeapp;
+package com.zybooks.fitme;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
